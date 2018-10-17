747a53c56de4b8d10739eae8645135fde2dcd992
Nazir Hajje <nhajje@hajje.com>
2018-10-17 17:07:30 +0300

    added execution runing time

--- LinkedList/LinkedListTest.java
+++ LinkedList/LinkedListTest.java
@@ -8,14 +8,19 @@
 public class LinkedListTest
 {
     public static void main(String[] args){
+        long startTime = System.nanoTime();
+
         LinkedList numberLL = new LinkedList();
         
         numberLL.deleteWithValue(2);
         
-        for (int i=1; i<=10; i++){
+        for (int i=1; i<=100000; i++){
             numberLL.append(i);
         }
-        
+        long endTime   = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("LinkedList - Appending 1,000,000 Int records: " + totalTime);        
+        /*
         numberLL.print();
         
         numberLL.deleteWithValue(100);
@@ -29,6 +34,7 @@ public class LinkedListTest
         numberLL.prepend(2);
         numberLL.prepend(8);
         numberLL.print();
+        */
     }
 
 }
